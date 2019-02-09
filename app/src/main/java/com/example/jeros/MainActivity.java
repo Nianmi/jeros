ec4143ada6691a2df81679b7bb02190000f65b8c
Mauro Stoffel <33456783+Mauro1CV@users.noreply.github.com>
2019-02-09 16:13:43 +0100

    First implementation of Contact retrieval

--- app/src/main/java/com/example/jeros/MainActivity.java
+++ app/src/main/java/com/example/jeros/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.jeros;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
+        ContentResolver resolver = getContentResolver();
+
+
+        Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        try{
+            while (cursor.moveToNext()) {
+                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                Log.i("name", id + " = " + name);
+
+                Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+
+                while (phoneCursor.moveToNext()) {
+                    String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    Log.i("number:", phoneNumber);
+                }
+            }}
+        catch(Exception e){
+
+        }
+        //Versuch mit spezifischer ID
+        int find = 12;
+        cursor.moveToFirst();
+        cursor.move(find-1);
+        String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+        String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+        Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+
+        //phoneCursor.moveToFirst();
+        //phoneCursor.move(find-1);
+        //String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+        Log.i("name", id + " = " + name);
+        //Log.i("number:", phoneNumber);
     }
 }
