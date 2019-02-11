3f4fc16b3efe320189ea5a9b1a0491c130b36e1e
Nianmi <Nianmi@users.noreply.github.com>
2019-02-11 02:26:25 +0100

    Sorted list alphabetically added Scrollview

--- app/src/main/java/com/example/jeros/MainActivity.java
+++ app/src/main/java/com/example/jeros/MainActivity.java
@@ -17,12 +17,17 @@ import android.util.Log;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,13 +35,15 @@ public class MainActivity extends AppCompatActivity {
     Button selectContact, testButton2, testButton3, testButton4;
     String[] contactNamesUse;
     boolean[] checkedItems;
-    ArrayList<ArrayList<String>> contacts;
-    ArrayList<String> contactsNames = new ArrayList<String>();
-    ArrayList<String> outputNames = new ArrayList<>();
+
     ArrayList<Integer> outputIDs = new ArrayList<>();
     ArrayList<ContactsClass> listContacts = new ArrayList<>();
     ArrayList<ContactsClass> listSelectedContacts = new ArrayList<>();
     ArrayList<ContactsClass> listShuffledContacts = new ArrayList<>();
+
+    //views
+    ScrollView scrollView;
+    LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +61,12 @@ public class MainActivity extends AppCompatActivity {
         testButton2 = (Button) findViewById(R.id.button2);
         testButton3 = (Button) findViewById(R.id.button3);
         testButton4 = (Button) findViewById(R.id.button4);
+
+        // Find the ScrollView
+          scrollView = (ScrollView) findViewById(R.id.scrollView1);
+        // Create a LinearLayout element
+        linearLayout = new LinearLayout(getApplicationContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
 
         selectContact.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
         ContacsButtonDe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                debugContacts();
+
             }
         });
 
@@ -126,13 +139,18 @@ public class MainActivity extends AppCompatActivity {
             mBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialogInterface, int which) {
+                    scrollView.removeAllViews();
+                    linearLayout.removeAllViews();
                     listSelectedContacts.clear();
                     for (int i = 0; i < outputIDs.size(); i++) {
                         Log.i("debug", "Name:" + listContacts.get(outputIDs.get(i)).getName());
                         Log.i("debug", "ID:" + listContacts.get(outputIDs.get(i)).getId());
                         Log.i("debug", "Nummer:" + listContacts.get(outputIDs.get(i)).getPhoneNumber1());
                         listSelectedContacts.add(listContacts.get(outputIDs.get(i)));
+                        addTextView(listContacts.get(outputIDs.get(i)).getName(),  listContacts.get(outputIDs.get(i)).getPhoneNumber1());
                     }
+                    // Add the LinearLayout element to the ScrollView
+                    scrollView.addView(linearLayout);
                 }
             });
 
@@ -144,6 +162,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    public void addTextView(String text, String phoneNumber){
+        EditText editText = new EditText(getApplicationContext());
+        editText.setKeyListener(null);
+        editText.setText(text + "\n" + phoneNumber);
+
+        linearLayout.addView(editText);
+    }
+
     public void testFinction2(){
         for (ContactsClass conti : listContacts) {
             Log.i("Id and Name: ", conti.getId() + conti.getName());
@@ -157,22 +184,22 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "You need at least 3 players", Toast.LENGTH_LONG).show();
         }
         else{
-            Log.i("SizeCheck", ""+listShuffledContacts.size());
+            //Log.i("SizeCheck", ""+listShuffledContacts.size());
             listShuffledContacts.clear();
-            Log.i("SizeCheck", ""+listShuffledContacts.size());
+            //Log.i("SizeCheck", ""+listShuffledContacts.size());
             listShuffledContacts.addAll(listSelectedContacts);
-            Log.i("SizeCheck", ""+listShuffledContacts.size());
+            //Log.i("SizeCheck", ""+listShuffledContacts.size());
             Collections.shuffle(listShuffledContacts);
             for(int i = 0; i< listShuffledContacts.size(); i++)
             {
                 if(i == 0)
                 {
                     sendSmS(listShuffledContacts.get(i).getName(),listShuffledContacts.get(listShuffledContacts.size()-1).getPhoneNumber1());
-                    Log.i("SMS: ", "1 Sending: " + listShuffledContacts.get(i).getName()+ " to " + listShuffledContacts.get(listShuffledContacts.size()-1).getName());
+                    //Log.i("SMS: ", "1 Sending: " + listShuffledContacts.get(i).getName()+ " to " + listShuffledContacts.get(listShuffledContacts.size()-1).getName());
                 }
                 else{
                     sendSmS(listShuffledContacts.get(i).getName(),listShuffledContacts.get(i-1).getPhoneNumber1());
-                    Log.i("SMS: ", "2 Sending: " + listShuffledContacts.get(i).getName()+ " to " + listShuffledContacts.get(i-1).getName());
+                    //Log.i("SMS: ", "2 Sending: " + listShuffledContacts.get(i).getName()+ " to " + listShuffledContacts.get(i-1).getName());
                 }
             }
         }
@@ -216,6 +243,15 @@ public class MainActivity extends AppCompatActivity {
 
                 listContacts.add(contactsClassTemp);
             }
+            //macht das ganza langsam
+            if (listContacts.size() > 0) {
+                Collections.sort(listContacts, new Comparator<ContactsClass>() {
+                    @Override
+                    public int compare(final ContactsClass object1, final ContactsClass object2) {
+                        return object1.getName().compareTo(object2.getName());
+                    }
+                });
+            }
         }
         catch (Exception e){
             boolean permissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED;
@@ -246,69 +282,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Du musch mir erlaubnis geh zum SMS schrieba du pur", Toast.LENGTH_LONG).show();
             }
         }
-    }
-
-    public void debugContacts() {
-        try {
-
-            ContentResolver resolver = getContentResolver();
-            ArrayList<ArrayList<String>> contacts = new ArrayList<ArrayList<String>>();
-
-            Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-
-            while (cursor.moveToNext()) {
-                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-
-                // Für ID + Namensausgabe
-                //Log.i("name", id + " = " + name);
-
-                Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
-
-                /* //Für Handy NUmmer Ausgabe
-                while (phoneCursor.moveToNext()) {
-                    String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                    Log.i("number:", phoneNumber);
-                }*/
-
-                ArrayList<String> temp = new ArrayList<String>();
-                temp.add(id);
-                temp.add(name);
-                contacts.add(temp);
-            }
-            //Versuch mit spezifischer ID
-            int find = 12;
-            cursor.moveToFirst();
-            cursor.move(find - 1);
-            String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
-            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-
-            Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                    new String[]{id}, null);
-
-            phoneCursor.moveToFirst();
-            String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-
-            Log.i("name", id + " = " + name);
-            Log.i("number:", phoneNumber);
-
-            //ArrayList check
-            for(int i = 0; i < contacts.size(); i++)
-            {
-                Log.i("id", contacts.get(i).get(0));
-                Log.i("name", contacts.get(i).get(1));
-            }
-
-        } catch (Exception e) {
-            boolean permissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED;
-            if (permissionGranted) {
-                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(getApplicationContext(), "Erlaubnis zum Kontaktläsa bruch i", Toast.LENGTH_LONG).show();
-            }
-        }
-
-
     }
 }
